1ced7a95338774e6efe9af5fb3f2d8e5b9c0c767
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-05-02 23:28:51 -0500

    Implementing Pn in MMs

--- src/js/CaseMMs.js
+++ src/js/CaseMMs.js
@@ -11,7 +11,7 @@ class CaseMMs extends QueueModel {
     }
 
     getLq() {
-        const lOverMToSOverFact = this.calculatePowerFactorialDivision(this.lOverM, this.s);
+        const lOverMToSOverFact = this.calculatePowerFactorialDivision(this.lOverM, this.s, this.s);
         const rhoComplement = 1 - this.getRho();
         return this.getP0() * lOverMToSOverFact * this.getRho() / rhoComplement / rhoComplement;
     }
@@ -29,27 +29,33 @@ class CaseMMs extends QueueModel {
     }
 
     getP0() {
-        const sPart = this.calculatePowerFactorialDivision(this.lOverM, this.s) * 1 / (1 - this.lOverM / this.s);
+        const sPart = this.calculatePowerFactorialDivision(this.lOverM, this.s, this.s) * 1 / (1 - this.lOverM / this.s);
         let sum = 0;
         for (let n = 0; n <= this.s - 1; n++)
-            sum += this.calculatePowerFactorialDivision(this.lOverM, n);
+            sum += this.calculatePowerFactorialDivision(this.lOverM, n, n);
         sum += sPart;
         return 1 / sum;
     }
 
     getPn(n) {
         if(n <= this.s)
-            return this.calculatePowerFactorialDivision(this.lOverM, n) * this.getP0()
+            return this.calculatePowerFactorialDivision(this.lOverM, n, n) * this.getP0();
         else
-            return 
+            return this.calculatePowerFactorialDivision(this.lOverM, n, this.s) / Math.pow(this.s, n - this.s) * this.getP0();
     }
 
-    calculatePowerFactorialDivision(base, n) {
-        if(n == 0)
-            return 1;
-        let result = base;
-        for (let i = 2; i <= n; i++)
+    calculatePowerFactorialDivision(base, exponent, factorial) {
+        let n = Math.min(exponent, factorial);
+        let result = 1;
+        let i;
+        for(i = 1; i <= n; i++)
             result *= base / i;
+        n = Math.max(exponent, factorial);
+        for(; i <= n; i++)
+            if(exponent > factorial)
+                result *= base;
+            else
+                result /= i;
         return result;
     }
 
