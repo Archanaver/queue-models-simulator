0cd2b3b42b22e3cc354d7a638d05122fc19e8a4a
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-05-04 21:03:27 -0500

    Added M/M/s and M/M/s/K inputs

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,28 +1,74 @@
-const mm1 = new CaseMM1(0, 0);
+const mm1 = new CaseMM1(3, 2);
+const mms = new CaseMMs(2, 3, 2);
+const mmsk = new CaseMMsK(1, 3, 2, 3);
 
 main();
 test();
 
 function main() {
     prepareMM1Handler();
+    prepareMMsHandler();
+    prepareMMsKHandler();
 }
 
 function prepareMM1Handler() {
-    const lambdaVal = document.querySelector("#mm1-form .l");
-    const muVal = document.querySelector("#mm1-form .m");
+    const model = mm1;
+    const lambdaVal = document.querySelector('#mm1-form .l');
+    const muVal = document.querySelector('#mm1-form .m');
     lambdaVal.addEventListener('input', (e) => {
-        mm1.lambda = Number(e.target.value);
-        updateAnswers(mm1);
-        console.log(mm1.lambda);
+        model.lambda = Number(e.target.value);
+        updateAnswers(model);
     });
     muVal.addEventListener('input', (e) => {
-        mm1.mu = Number(e.target.value);
-        updateAnswers(mm1);
+        model.mu = Number(e.target.value);
+        updateAnswers(model);
+    });
+}
+
+function prepareMMsHandler() {
+    const model = mms;
+    const lambdaVal = document.querySelector('#mms-form .l');
+    const muVal = document.querySelector('#mms-form .m');
+    const sVal = document.querySelector('#mms-form .s');
+    lambdaVal.addEventListener('input', (e) => {
+        model.lambda = Number(e.target.value);
+        updateAnswers(model);
+    });
+    muVal.addEventListener('input', (e) => {
+        model.mu = Number(e.target.value);
+        updateAnswers(model);
+    });
+    sVal.addEventListener('input', (e) => {
+        model.s = Number(e.target.value);
+        updateAnswers(model);
+    });
+}
+
+function prepareMMsKHandler() {
+    const model = mmsk;
+    const lambdaVal = document.querySelector('#mmsk-form .l');
+    const muVal = document.querySelector('#mmsk-form .m');
+    const sVal = document.querySelector('#mmsk-form .s');
+    const kVal = document.querySelector('#mmsk-form .k');
+    lambdaVal.addEventListener('input', (e) => {
+        model.lambda = Number(e.target.value);
+        updateAnswers(model);
+    });
+    muVal.addEventListener('input', (e) => {
+        model.mu = Number(e.target.value);
+        updateAnswers(model);
+    });
+    sVal.addEventListener('input', (e) => {
+        model.s = Number(e.target.value);
+        updateAnswers(model);
+    });
+    kVal.addEventListener('input', (e) => {
+        model.k = Number(e.target.value);
+        updateAnswers(model);
     });
 }
 
 function updateAnswers(queueModel) {
-    console.log('Updating element');
     const lText = document.getElementById('l');
     const lqText = document.getElementById('lq');
     const wText = document.getElementById('w');
